16bd0c470e8b74b5a739e63becab70befd467b9a
Joseph Meleshko <jmeleshk@ualberta.ca>
2018-09-07 23:50:37 -0600

    changed json files, made everything look better

--- D&DBot.py
+++ D&DBot.py
@@ -1,19 +1,45 @@
 import discord, json, random, re
 
+schools = {"A":"Abjuration", "C":"Conjuration", "D":"Divination", "E":"Enchantment", "V":"Evocation", "I":"Illusion", "N":"Necromancy", "T":"Transmutation"}
+
 def cleanText(text):
     p = re.compile(r'<.*?>')
     return p.sub('', text)
 
-def printSpell(spell):
-    print("Name:", spell["name"], "\n")
-    print("Description:", spell["desc"])
-    print("Range:", spell["range"], "\n")
-    print("Duration:", spell["duration"], "\n")
-    print("Page:", spell["page"], spell["source"], "\n")
+def componentParsing(components):
+    text = ""
+    for key in components.keys():
+        if isinstance(components[key], str):
+            text = text + "M (" + components[key] + ")"
+        else:
+            text = text + key.upper() + " "
+    return text
 
-def printSpellDetailed(spell):
-    for key in spell.keys():
-        print(key, "-", spell[key])
+def entriesParsing(entries):
+    text = ""
+    for obj in entries:
+        if isinstance(obj, str):
+            text = text + obj + "\n"
+        else:
+            if obj["type"] == "entries":
+                text = text + "**" + obj["name"] + "** - " + "\n".join(obj["entries"]) + "\n"
+            else:
+                text = text + "****Special, See Guide****\n"
+    return text
+
+def spellText(spell):
+    return "**Name:** " + spell["name"] + "\n" + \
+        "**Type:** Level " + str(spell["level"]) + " " + schools[spell["school"]] + "\n" + \
+        "**Casting Time:** " + str(spell["time"][0]["number"]) + " " + spell["time"][0]["unit"] + "\n" + \
+        "**Range:** " + \
+        ("" if spell["range"]["distance"]["type"] in ["touch", "self", "special", "sight", "unlimited"] else str(spell["range"]["distance"]["amount"])) + \
+        " " + spell["range"]["distance"]["type"] + "\n" + \
+        "**Components:** " + componentParsing(spell["components"]) + "\n" + \
+        "**Duration:** " + (spell["duration"][0]["type"] if spell["duration"][0]["type"] in ["instant", "permanent", "special"] \
+        else str(spell["duration"][0]["duration"]["amount"]) + " " + spell["duration"][0]["duration"]["type"]) + "\n" + \
+        "**Description:** " + entriesParsing(spell["entries"]) + \
+        (entriesParsing(spell["entriesHigherLevel"]) if "entriesHigherLevel" in spell.keys() else "") + \
+        "**Book:** " + spell["source"]
 
 def randomSpell(spells):
     return random.choice(spells)
@@ -22,12 +48,12 @@ def runServer():
     spells = json.load(open("spells.json"))
     print("Loaded all availible spells")
 
-    PHBSpells = [spell for spell in spells if spell["source"] == "PHB"]
-    print("Filtered to Player's Hand Book")
+    spells = [spell for spell in spells if spell["source"] == "PHB" or spell["source"] == "XGE"]
+    print("Filtered to Player's Hand Book and Xanathar's Guide To Everything")
 
-    for spell in PHBSpells:
-        spell["desc"] = spell["desc"].replace("<p>", "").replace("</p>", "\n")
-    print("Cleaned spell descriptions")
+    for spell in spells:
+        print(spell["name"])
+        print(spellText(spell))
 
     token = "NDg3ODAzODkwNDExMjQxNDgy.DnTAOg.bBEPj7Lg7UBv9LV0_ARbQht3akI"
     client = discord.Client()
@@ -37,42 +63,41 @@ def runServer():
     async def on_message(message):
         channel = message.channel
 
-        print("Got Command:", message.content)
-        toSend = ""
-
         if message.author == client.user:
             return
 
         elif message.content.startswith('#'):
             return
 
-        elif message.content.startswith("roll"):
+        print("Got Command:", message.content)
+        toSend = ""
+
+        if message.content.startswith("roll"):
             try:
                 command = message.content[4:]
-                count, die = int(command[:command.index("d")]), int(command[command.index("d")+1:])
+                count, die = int(command[:command.index("d")]), int(command[(command.index("d")+1):])
                 rolls = list()
                 sum = 0
                 for i in range(count):
                     roll = random.randrange(die) + 1
                     rolls.append(roll)
                     sum += roll
-                toSend = "Result: " + sum + "\n" + rolls
-
+                toSend = "Result: " + str(sum) + "\n" + str(rolls)
             except:
                 await client.send_message(channel, "Unknown Roll Command")
                 return
 
         elif message.content in ["random", "r", "Random"]:
-            toSend = str(randomSpell(PHBSpells))
+            toSend = spellText(randomSpell(spells))
 
         else:
             for spell in spells:
-                if spell["name"] == message.content:
-                    toSend = str(spell)
+                if spell["name"].lower().replace("'", "").replace(" ", "") == message.content.lower().replace("'", "").replace(" ", ""):
+                    toSend = spellText(spell)
 
         print("Responding With:", toSend)
         toSend = cleanText(toSend)
-        if len(toSend) == "":
+        if toSend == "":
             await client.send_message(channel, "Unknown Command")
             return
         elif len(toSend) < 2000:
